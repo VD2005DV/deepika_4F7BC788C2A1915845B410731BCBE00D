a5b928f7a80f2fde0cc72c416147cc907b761fbb
VD2005DV <144783266+VD2005DV@users.noreply.github.com>
2023-10-14 18:51:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-def Check_Leap(Year):
-if((year%400==0)or
-   (year%100!=0)and
-   (year%4==0)):
-print("Given year is a leap year");
-else:
-print("Given year is not a leap year");
-Year=int(input("Enter the number:"))
-Check_leap(Year)
+க்கிற்class Player:
+
+  def play(self):
+    print("The player is playing cricket.")
+
+
+class Batsman(Player):
+
+  def play(self):
+    print("Thw batsman is batting.")
+
+
+class Bowler(Player):
+
+  def play(self):
+    print("The bowler is bowling.")
+
+
+batsman = Batsman()
+bowler = Bowler()
+batsman.play()
+bowler.play()
